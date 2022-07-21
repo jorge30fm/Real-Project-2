e842084168ee171a518d9fe477623033caa81c94
Jorge Felix <jorge30fm@gmail.com>
2022-07-21 17:21:15 -0400

    add user seeds

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+const { User, Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// get all users
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//get single user by id
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ["id", "title", "post_text", "image", "created_at"],
+      },
+    ],
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+module.exports = router;
