f6256214c45b08f485f3b58742fa4e647dd08ab4
Jorge Felix <jorge30fm@gmail.com>
2022-07-26 22:35:21 -0400

    implemented cloudinary and display images on dashboard

--- public/javascript/new-post.js
+++ public/javascript/new-post.js
@@ -1,27 +1,58 @@
+
+const api_key = "523429575836714";
+const cloud_name = "degnyzbus";
+
 const newFormHandler = async function (event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[name="post-title"]').value;
-    const post_text = document.querySelector('input[name="post-text"]').value;
-    const image = document.getElementById('post-image');
-  
-    const response = await fetch(`/api/post`, {
-      method: "POST",
-      body: JSON.stringify({
-        title,
-        post_text,
-        image,
-      }),
-  
-      headers: { "Content-Type": "application/json" },
-    });
-    if (response.ok) {
-      document.location.replace("/dashboard");
-    } else {
-      alert(response.statusText);
-    }
-  };
-  
-  document
-    .querySelector(".new-post-form")
-    .addEventListener("submit", newFormHandler);
+	event.preventDefault();
+
+	const title = document.querySelector('input[name="post-title"]').value;
+	const post_text = document.querySelector('input[name="post-text"]').value;
+
+	//get signature,  good for an hour
+	const signatureResponse = await axios.get("api/post/get-signature");
+
+	const data = new FormData();
+	data.append("file", document.querySelector("#post-image").files[0]);
+	data.append("api_key", api_key);
+	data.append("signature", signatureResponse.data.signature);
+	data.append("timestamp", signatureResponse.data.timestamp);
+
+	const cloudinaryResponse = await axios.post(
+		`https://api.cloudinary.com/v1_1/${cloud_name}/auto/upload`,
+		data,
+		{
+			headers: { "Content-Type": "multipart/form-data" },
+			onUploadProgress: function (event) {
+				console.log(event.loaded / event.total);
+			},
+		}
+	);
+	console.log(cloudinaryResponse);
+
+	const public_id = cloudinaryResponse.data.public_id;
+	const version = cloudinaryResponse.data.version;
+	const signature = cloudinaryResponse.data.signature;
+
+	const response = await fetch(`/api/post`, {
+		method: "POST",
+		body: JSON.stringify({
+			title,
+			post_text,
+			public_id,
+			version,
+			signature,
+		}),
+
+		headers: { "Content-Type": "application/json" },
+	});
+	if (response.ok) {
+		document.location.replace("/dashboard");
+	} else {
+		alert(response.statusText);
+	}
+}
+
+	document
+		.querySelector(".new-post-form")
+		.addEventListener("submit", newFormHandler);
+
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -5,8 +7,11 @@ const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const routes = require('./controllers');
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -37,3 +42,10 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+
+
+
+
+
+
